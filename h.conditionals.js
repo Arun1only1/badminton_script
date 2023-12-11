d205c2f08805a29a330ef09558b70fa435779b1b
Arun1only1 <arun1nly1@gmail.com>
2023-12-11 14:32:22 +0545

    switch and ternary

--- h.conditionals.js
+++ h.conditionals.js
@@ -93,23 +93,183 @@
 //   console.log("z is greatest");
 // }
 
-let a = 5;
-let b = 2;
+// let a = 5;
+// let b = 2;
 
-let option = "multiply";
+// let option = "multiply";
 
-if (option === "add") {
-  console.log(a + b);
-} else if (option === "subtract") {
-  console.log(a - b);
-} else if (option === "multiply") {
-  console.log(a * b);
-} else if (option === "divide") {
-  console.log(a / b);
-} else if (option === "power") {
-  console.log(a ** b);
-} else if (option === "remainder") {
-  console.log(a % 2);
-} else {
-  console.log("Invalid option");
-}
+// if (option === "add") {
+//   console.log(a + b);
+// } else if (option === "subtract") {
+//   console.log(a - b);
+// } else if (option === "multiply") {
+//   console.log(a * b);
+// } else if (option === "divide") {
+//   console.log(a / b);
+// } else if (option === "power") {
+//   console.log(a ** b);
+// } else if (option === "remainder") {
+//   console.log(a % 2);
+// } else {
+//   console.log("Invalid option");
+// }
+
+// let marks = 90;
+
+// let grade;
+
+// if (marks >= 90) {
+//   grade = "A+";
+// } else if (marks >= 80) {
+//   grade = "A";
+// } else if (marks >= 70) {
+//   grade = "B";
+// } else if (marks >= 60) {
+//   grade = "C";
+// } else {
+//   grade = "F";
+// }
+
+// console.log(grade);
+
+// ? switch => multiple choice questions
+
+// switch(condition){
+// case 0:
+// some code here
+// break;
+// }
+
+// polarity of a number
+// +ve, -ve ,0
+
+// let num = 25;
+
+// if (num > 0) {
+//   console.log("+ve");
+// } else if (num < 0) {
+//   console.log("-ve");
+// } else {
+//   console.log("zero");
+// }
+
+//limitation: switch always handles cases for equality
+
+// let day = 70;
+
+// if (day === 1) {
+//   console.log("Sunday");
+// } else if (day === 2) {
+//   console.log("Monday");
+// } else if (day === 3) {
+//   console.log("Tuesday");
+// } else if (day === 4) {
+//   console.log("Wednesday");
+// } else if (day === 5) {
+//   console.log("Thursday");
+// } else if (day === 6) {
+//   console.log("Friday");
+// } else if (day === 7) {
+//   console.log("Saturday");
+// } else {
+//   console.log("Invalid day number");
+// }
+
+// let day = 1;
+// switch (day) {
+//   case 1:
+//     console.log("Sunday");
+//     break;
+
+//   case 2:
+//     console.log("Monday");
+//     break;
+//   case 3:
+//     console.log("Tuesday");
+//     break;
+//   case 4:
+//     console.log("Wednesday");
+//     break;
+//   case 5:
+//     console.log("Thursday");
+//     break;
+
+//   case 6:
+//     console.log("Friday");
+//     break;
+
+//   case 7:
+//     console.log("Saturday");
+//     break;
+
+//   default:
+//     console.log("Invalid day number");
+// }
+
+// let x = 4;
+// let y = 2;
+
+// let option = "multiply";
+
+// switch (option) {
+//   case "add":
+//     console.log(x + y);
+//     break;
+
+//   case "subtract":
+//     console.log(x - y);
+//     break;
+
+//   case "multiply":
+//     console.log(x * y);
+//     break;
+
+//   case "divide":
+//     console.log(x / y);
+//     break;
+
+//   default:
+//     console.log("Invalid option");
+// }
+
+// ?ternary
+// ?
+
+// syntax
+// condition?true_case:false_case
+// let num = 5;
+
+// let remainder = num % 2;
+
+// if (remainder === 0) {
+//   console.log("even");
+// } else {
+//   console.log("odd");
+// }
+
+// remainder === 0 ? console.log("even") : console.log("odd");
+
+// temp greater than or equals to  40 => print "Hot day" else print "Cool day"
+// let temp = 30;
+
+// if (temp >= 40) {
+//   console.log("Hot day");
+// } else {
+//   console.log("Cool day");
+// }
+
+// temp >= 40 ? console.log("Hot day") : console.log("Cool day");
+
+// check polarity of number using if..else , using ternary chaining
+
+// let x = -20;
+
+// if (x > 0) {
+//   console.log("+ve");
+// } else if (x < 0) {
+//   console.log("-ve");
+// } else {
+//   console.log("zero");
+// }
+
+// x > 0 ? console.log("+ve") : x < 0 ? console.log("-ve") : console.log("zero");
