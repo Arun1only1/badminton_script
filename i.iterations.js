80f8af777990c06ce58ef6c603de2181de5735d8
Arun1only1 <arun1nly1@gmail.com>
2023-12-11 14:32:32 +0545

    intro to loops

---
+++ i.iterations.js
@@ -0,0 +1,15 @@
+// iteration
+// loop => repetition
+
+// for
+// while
+// do..while
+
+// syntax
+// for(initial_value;condition;increment/decrement){
+// some code here
+// }
+
+for (let i = 0; i < 3; i = i + 1) {
+  console.log("hello");
+}
