950140613afcd5a27ee85d59fa4fe9f6a5a2554f
Arun1only1 <arun1nly1@gmail.com>
2023-12-13 14:49:33 +0545

    while and do while

--- i.iterations.js
+++ i.iterations.js
@@ -129,10 +129,10 @@
 // increment/decrement
 // }while(condition)
 
-let i = 1;
+// let i = 1;
 
-do {
-  console.log(i);
+// do {
+//   console.log(i);
 
-  i++;
-} while (i <= -100);
+//   i++;
+// } while (i <= -100);
