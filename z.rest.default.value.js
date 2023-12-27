1409254a294715e7361410ea854317f3eef9da72
Arun1only1 <arun1nly1@gmail.com>
2023-12-27 14:42:12 +0545

    default and rest operators, asynchronous js and event loop

---
+++ z.rest.default.value.js
@@ -0,0 +1,28 @@
+//? rest operator => collects value into an array
+
+// syntax
+//  ...
+
+// const calculateSum = (...otherValues) =>
+//   otherValues.reduce((sum, item) => sum + item, 0);
+
+// const res = calculateSum(2, 3, 5, 7, 9, 11, 71, 42);
+
+// console.log(res);
+
+//? default value
+
+// const getProduct = (num1 = 1, num2 = 1) => {
+//   let product = num1 * num2;
+
+//   return product;
+// };
+
+// const result = getProduct();
+// console.log(result);
+
+// const sayHello = (firstName = "New User") => {
+//   console.log(`Hello ${firstName}`);
+// };
+
+// sayHello("Deepesh");
---
+++ z1.asynchronous.behaviour.in.node.js
@@ -0,0 +1,27 @@
+// node => js runtime
+// node => asynchronous
+// synchronous => occurring at same time
+// asynchronous => not occurring at same time
+// node => single threaded
+//  being single threaded, there is chance of blocking of code execution flow
+// but node handles it using event loop
+// node implements  non-blocking using event loop
+
+console.log("Hi");
+
+setTimeout(() => {
+  console.log("Timeout 1");
+}, 5000);
+
+setTimeout(() => {
+  console.log("Timeout 2");
+}, 1000);
+
+setTimeout(() => {
+  console.log("Timeout 3");
+}, 0);
+
+console.log("Hello");
+
+// Stack => LIFO => Last In First Out
+// Queue => FIFO => First In First Out
